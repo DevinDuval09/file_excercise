d0b80d0cca5c07229533f403a3cf717f5732264d
Devin Duval <devinduval09@gmail.com>
2020-11-13 16:01:01 -0800

    os_copy_binary working

--- file_exercise.py
+++ file_exercise.py
@@ -18,11 +18,26 @@ class Os_Directory():
         file_name = str(self.files[int(input(
                         self.get_wd(header="Select a file from ") + '\n:'))])
         new_path = input('Enter target directory:')
-        print(new_path)
+
         with open(file_name, 'r') as file_source,\
                 open(new_path + '\\' + file_name, 'w') as file_copy:
             file_copy.write(''.join(file_source.readlines()))
             file_copy.close()
+
+    def os_copy_binary(self):
+        file_name = str(self.files[int(input(
+                        self.get_wd(header="Select a file from ") + '\n:'))])
+        new_path = input('Enter target directory:')
+
+        with open(file_name, 'rb') as file_source,\
+            open(new_path + '\\' + file_name, 'wb') as file_copy:
+            while True:
+                buf = file_source.read(1024)
+                if buf:
+                    file_copy.write(buf)
+                else:
+                    break
+
 
     def pwd(self):
         print(self.get_wd())
